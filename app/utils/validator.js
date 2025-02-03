6761ec1af83207327cead5e7e42cecfa90556fa3
leenaa46 <hsherlock22@gmail.com>
2025-02-03 15:13:41 +0700

    clear cache

--- app/utils/validator.js
+++
@@ -1,7 +0,0 @@
-import {
-    validate
-} from 'express-validation'
-
-exports.validateDefault = (rule) => {
-    return validate(rule, { context: false, statusCode: 422, keyByField: true }, { abortEarly: false })
-}
--- scrypt/migrate_fresh.js
+++
@@ -1,7 +0,0 @@
-import db from '../models'
-
-db.sequelize.sync({
-  force: true
-}).then(() => {
-  console.log('refresh database success');
-})
