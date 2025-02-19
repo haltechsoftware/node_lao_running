00a2363b7f3988a7b77700d5acb92fd0060ff5c3
leenaa46 <hsherlock22@gmail.com>
2025-02-19 10:14:43 +0700

    Remove unnecessary console log from OTP verification helper

--- app/helpers/otp.helper.js
+++ app/helpers/otp.helper.js
@@ -2,9 +2,7 @@ import jwt from 'jsonwebtoken'
 
 exports.verify = (id_token) => {
   const decodeData = jwt.decode(id_token)
-  
   const seconds = new Date().getTime() / 1000;
-  console.log( decodeData.iss , process.env.F_ISS);
 
   if (
     !decodeData
