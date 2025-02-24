0cfc3a849c28e400f842198fd5ae935ffc32d34a
leenaa46 <hsherlock22@gmail.com>
2025-02-24 16:50:26 +0700

    Add ESLint rule to warn on unused variables

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,5 +5,10 @@ import pluginJs from "@eslint/js";
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {languageOptions: { globals: {...globals.browser, ...globals.node} }},
-  pluginJs.configs.recommended
+  pluginJs.configs.recommended,
+  {
+    rules: {
+      "no-unused-vars": "warn"
+    }
+  }
 ];
