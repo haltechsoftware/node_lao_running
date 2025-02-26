42ca7ec2f37d0af9cb02524729bf2a258417942d
leenaa46 <hsherlock22@gmail.com>
2025-02-26 10:34:07 +0700

    Update package names and prices in seed data

--- seeders/20210823043617-packages.js
+++ seeders/20210823043617-packages.js
@@ -6,14 +6,26 @@ module.exports = {
       "packages",
       [
         {
-          name: "Starter",
-          price: process.env.NODE_ENV == "production" ? 149000 : 2,
+          name: "40KM/240,000",
+          price: 240000,
           createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
-          name: "Pro",
-          price: process.env.NODE_ENV == "production" ? 199000 : 3,
+          name: "40KM/440,000",
+          price: 440000,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          name: "90KM/290,000",
+          price: 290000,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          name: "90KM/490,000",
+          price: 490000,
           createdAt: new Date(),
           updatedAt: new Date(),
         },
